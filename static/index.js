24428d7c4a290dc6979024bf1b186e4cb7252153
Sai Bade <90086494+saibade2@users.noreply.github.com>
2023-10-09 15:58:26 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,56 +1,43 @@
-var date = new Date()
-let display_date= "Date:" + date.toLocaleDateString()
+$(document).ready(function(){
+    console.log('Ready')
 
-$(document).ready(function () {
-    $("#display_date").html(display_date)
-    $('#save_button').prop('disabled', true);
-})
+    let date_time = new Date()
+    let current_date = date_time.toLocaleDateString()
 
-let predicted_emotion;
-$(function () {
-    $("#predict_button").click(function () {
-        let input_data = {
-            "text": $("#text").val()
-        }
+    $('#date').text("Date : " + current_date)
+
+    $('#button').click(function(){
+
+        let review = $('#text').val()
+        console.log(review)
+
+        let input_data = {'customer_review' : review}
+        console.log(input_data)
+
         $.ajax({
-            type: 'POST',
-            url: "/predict-emotion",
-            data: JSON.stringify(input_data),
-            dataType: "json",
-            contentType: 'application/json',
-            success: function (result) {
-                $("#prediction").html(result.data.predicted_emotion)
-                $("#emo_img_url").attr('src', result.data.predicted_emotion_img_url);
-                $('#prediction').css("display", "");
-                $('#emo_img_url').css("display", "");
-                predicted_emotion = result.data.predicted_emotion
-                $('#save_button').prop('disabled', false);
-            },
-            error: function (result) {
-                alert(result.responseJSON.message)
-            }
-        });
-    });
-    $("#save_button").click(function () {
-        save_data = {
-            "date": display_date,
-            "text": $("#text").val(),
-            "emotion": predicted_emotion
-        }
-        $.ajax({
-            type: 'POST',
-            url: "/save-entry",
-            data: JSON.stringify(save_data),
-            dataType: "json",
-            contentType: 'application/json',
-            success: function () {
-                alert("Your entry has been saved successfully!")
-                window.location.reload()
-            },
-            error: function (result) {
-                alert(result.responseJSON.message)
-            }
-        });
 
-    });
+            url : "/predict",
+            type : "POST",
+            data : JSON.stringify(input_data),
+            dataType : 'json',
+            contentType : 'application/json',
+            success : function(result){
+                let prediction = result.prediction
+                let emoji_url = result.url
+                console.log(emoji_url)
+
+                $('#sentiment').text(prediction)
+                $('#sentiment').show()
+
+                $('#emoji').attr('src' , emoji_url)
+                $('#emoji').show()
+            },
+            error : function(result){
+                console.log(result)
+            }
+        })
+
+        $('#text').val("")
+        
+    })
 })
